bb595cc26a0f03af438672fa61b3c09cbe202205
Rory Davies <roryjd123@gmail.com>
2020-10-10 18:50:32 +1300

    Updates

---
+++ precache-manifest.59e2989a5ef4eae516cb006639e9d2ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea4a50e921759a1c26c610b59e61475",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9306655d86ab6b567d82",
+    "url": "/static/css/2.50373daa.chunk.css"
+  },
+  {
+    "revision": "f6e4c0183e2c00dbe1bb",
+    "url": "/static/css/main.81aba850.chunk.css"
+  },
+  {
+    "revision": "9306655d86ab6b567d82",
+    "url": "/static/js/2.e6c5f58a.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.e6c5f58a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e4c0183e2c00dbe1bb",
+    "url": "/static/js/main.d8917d30.chunk.js"
+  },
+  {
+    "revision": "9f326db6e5ffa618a0c9",
+    "url": "/static/js/runtime-main.7409b536.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
